a7b71efb8459f5e537209173dcb30a319eb13f68
esraagamal <esraagamal506@gmail.com>
2020-06-24 00:25:19 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,8 +2,6 @@ const con1=document.getElementById('con-1');
 const con2=document.getElementById('con-2');
 const con3=document.getElementById('con-3');
 const img=document.getElementById('img-slider'); 
-
-
 var images=["images/image-john.jpg","images/image-tanya.jpg"];
 var cont1=["“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
 "“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”"]
