2af74bf143f7c2e7f5a664167fa57deddb40ca9b
esraagamal <esraagamal506@gmail.com>
2020-06-26 22:24:27 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,43 +1,30 @@
-const con1=document.getElementById('con-1');
-const con2=document.getElementById('con-2');
-const con3=document.getElementById('con-3');
+const slide_1=document.getElementById('slider-1');
+const slide_2=document.getElementById('slider-2');
 const img=document.getElementById('img-slider'); 
-var images=["images/image-john.jpg","images/image-tanya.jpg"];
-var cont1=["“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”",
-"“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”"]
-var cont2=["John Tarkpor","Tanya Sinclair"];
-var cont3=["Junior Front-end Developer"," UX Engineer"];
-var i=images.length;
+var images=["images/image-tanya.jpg","images/image-john.jpg"];
+const slider=[slide_1,slide_2];
+var i=0;
 function nextBtn()
 {
-    if(i<images.length)
-    {
-      
+    slider[i].style.display="none";
+    if(i<images.length-1){
         i++;
     }
-   
     else{
-      
-        i=1;
-       
+        i=0;
     }
-
-    con1.innerText=cont1[i-1];
-    con2.innerText=cont2[i-1];
-    con3.innerText=cont3[i-1];
-    img.src=images[i-1];
+    slider[i].style.display="block";
+    img.src=images[i];
 }
 function preBtn(){
-    if(i<images.length+1 && i>1)
-    {
-        i--;
-    }
-    else{
-        i=images.length;
-    }
-    con1.innerText=cont1[i-1];
-    con2.innerText=cont2[i-1];
-    con3.innerText=cont3[i-1];
-    img.src=images[i-1];
+    slider[i].style.display="none";
+   if(i===0){
+    i=1;
+   }
+   else{
+    i--;
+   }
+    slider[i].style.display="block";
+    img.src=images[i];
 }
 
